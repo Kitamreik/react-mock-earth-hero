26b186172dcd5a2306f657c999fe9a25d53e015e
kitamreik <kitdamreik@gmail.com>
2022-10-26 21:15:02 -0600

    mock MVP done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Routes, Route, useNavigate} from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 
 /*
 // The useNavigate hook returns a function that lets us navigate programmatically, e.g. after a form is submitted or a button is clicked.
@@ -24,6 +25,34 @@ import SignUpForm from './components/SignUpForm';
 export default function App() {
   const navigate = useNavigate();
 
+  // template
+  // const navigateToFunction = () => {
+  //   navigate('/path', {replace: true});
+  // };
+
+  /*
+   // When the replace property on the options object is set to true, the current entry in the history stack gets replaced with the new one.
+
+  // In other words, navigating to the new route won't push a new entry into the history stack, so if the user clicks the back button, they won't be able to navigate to the previous page.
+  // For example, this is useful when a user logs in, because you don't want them to be able to click the back button and get back to the login page.
+
+  // It is also useful if you have a route that redirects users to a different page, because you don't want users to click the back button and get redirected again.
+
+// Add'l documentation: https://v5.reactrouter.com/web/guides/quick-start
+  */ 
+ 
+const navigateToGetInvolved = () => {
+    navigate('/get-involved', {replace: true});
+  };
+
+const navigateToDonate = () => {
+    navigate('/donate', {replace: true});
+  };
+
+  const navigateToTeam = () => {
+    navigate('/team', {replace: true});
+  };
+
   const navigateToGroups = () => {
     navigate('/groups', {replace: true});
   };
@@ -40,28 +69,19 @@ export default function App() {
     navigate('/app', {replace: true});
   };
   
-  /*
-   // When the replace property on the options object is set to true, the current entry in the history stack gets replaced with the new one.
-
-  // In other words, navigating to the new route won't push a new entry into the history stack, so if the user clicks the back button, they won't be able to navigate to the previous page.
-  // For example, this is useful when a user logs in, because you don't want them to be able to click the back button and get back to the login page.
-
-  // It is also useful if you have a route that redirects users to a different page, because you don't want users to click the back button and get redirected again.
-
-// Add'l documentation: https://v5.reactrouter.com/web/guides/quick-start
-  */ 
- 
   const navigateHome = () => {
     // 👇️ navigate to /
     navigate('/');
   };
 
+  // Components that Render --> "Route"
   function Home() {
     return (
     <>
       <h2>Home</h2>
       <h3>Take action for people & planet</h3>
       <p>Respond to climate change by discovering personalised, positive steps we can take to care for our planet and communities. It's our future to choose</p>
+      <img src='.././earth-hero-phone.png' alt='EH on mobile'></img>
     </>
     ) 
   }
@@ -135,6 +155,98 @@ export default function App() {
     )
   }
 
+  function Team() {
+    return (
+      <>
+        <h2>Team</h2>
+        <h3>We Are Earth Heroes</h3>
+        <p>We believe a billion compassionate activists can restore a healthy earth where life can thrive, so we are motivated to inspire people and their communities to join the global climate movement.
+
+        Our nonprofit volunteer team is focused on providing tools and ideas that empower all who want to reduce emissions and care for our shared planet. We help people to effectively respond to the climate emergency while strengthening their resilience and quality of life.</p>
+
+      <img src='.././team.png' alt='team members of EH'></img>
+
+      <img src='.././team-2.png' alt='team members of EH'></img>
+
+      <img src='.././team-3.png' alt='team members of EH'></img>
+      </>
+
+    )
+  }
+
+  function GetInvolved() {
+    return (
+      <>
+        <h1>Make An Impact on a Global Scale</h1>
+        <p>Earth Hero is a group of volunteers who came together to do our part to address the climate crisis. We contribute in different ways with our own unique interests -- yet share a common cause.
+
+        Many hands make light work and accelerates change. If you are interested, and can spare at least 20 hours per month, send a message in the contact form below.
+
+        You can check out the positions we are actively recruiting for here.
+        </p>
+
+        <h3>Ways to Get Involved</h3>
+        <ul>
+          <ol>Writing Actions, Articles & Other Content</ol>
+          <ol>Website Dev (Javascript React/Gatsby)</ol>
+          <ol>Carbon Pollution Modeling</ol>
+          <ol>Research</ol>
+          <ol>Translations</ol>
+          <ol>Social Media Support</ol>
+          <ol>UX/UI Design</ol>
+          <ol>Art or Graphic Design</ol>
+          <ol>App Development (Swift and Kotlin)</ol>
+          <ol>Any other way that interests you!</ol>
+        </ul>
+        <br />
+        <Contacts />
+
+      </>
+    )
+  }
+
+  function Donate () {
+    return (
+      <>
+        <h1>Support Our Mission</h1>
+        <p>Together, we can create a more sustainable future.</p>
+        <Button style={{backgroundColor: "green"}}>DONATE</Button>
+        <br />
+        <br />
+        
+        <h2>Why Donate?</h2>
+        <p>Earth Hero is a non-profit team of volunteers working to address the climate emergency and improve our shared planet. Your support helps us increase the positive impact and cover our costs.</p>
+        <br />
+        
+        <h2>Donate to empower action.</h2>
+        <img src='' alt=''></img>
+        <p>
+        Earth Hero provides tools and resources that help tens of thousands act as change agents.</p>
+        <br />
+
+        <h2>Donate to make the global, personal. </h2>
+        <img src='' alt=''></img>
+        <p>Earth hero envisions a future of one billion activists committed to restoring a healthy Earth where life can thrive.</p>
+        <br />
+        
+        <h2>Donate to improve lives</h2>
+        <img src='' alt=''></img>
+        <p>Whether adjusting your mode of transportation or your eating habits, Earth Hero provides personalized guidance to help people lead more fulfilling and sustainable lives.</p>
+        <br />
+
+        <h2>Donate because this depends on it.</h2>
+        <img src='' alt=''></img>
+        <p>
+        This planet needs you. We cannot do this alone. A collective effort is necessary so future generations can enjoy this world.</p>
+
+        <br />
+        <h2>Where does your money go?</h2>
+        <p>At Earth Hero, we are committed to acting as ethical stewards of your gifts. Your support ensures that Earth Hero is 100% free for everyone around the world. It funds the technology and tools we use to create, improve, distribute, and spread the message about better ways of living.</p>
+        <Button style={{backgroundColor: "green"}}>DONATE</Button>
+      </>
+    )
+  }
+
   // 404 Error- EXPERIMENTAL CODE
   /*
   function NotFound() {
@@ -148,7 +260,7 @@ export default function App() {
   }
   */ 
   
-
+  // What renders as the entry point
   return (
     <div className='App'>
       <Container>
@@ -161,22 +273,23 @@ export default function App() {
           <div className='Routes'>
             {/* Link from react-router-dom --> we could use Link, but we won't here */}
             <Button onClick={navigateHome}>Home</Button>
-            <Button onClick={navigateToContacts}>Contacts</Button>
             {/* EXPERIMENTAL CODE */}
             <Button onClick={navigateToApp}>App</Button>
             <Button onClick={navigateToActions}>Actions</Button>  
             <Button onClick={navigateToGroups}>Groups</Button>
-
-            {/* 
-            <Button onClick={navigateToTeam}>Route</Button>
-            <Button onClick={navigateToDonate}>Route</Button> */}
-            {/* --------------------- */}
+            <Button onClick={navigateToTeam}>Team</Button>
+            <Button onClick={navigateToGetInvolved}>Get Involved </Button>
+            <Button onClick={navigateToContacts}>Contact</Button>
+            <Button onClick={navigateToDonate}>Donate</Button> 
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/app" element={<App />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/actions" element={<Actions />} />
               <Route path="/groups" element={<Groups />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/get-involved" element={<GetInvolved />} />
+              <Route path="/donate" element={<Donate />} />
               {/* <Route component={NotFound} /> - EXPERIMENTAL CODE */}
             </Routes>
           </div>
